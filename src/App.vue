3ba9646415d7593097383ce4edc4e562b8aeba03
Ravi Chandra Anagani <ravi.chandra737@gmail.com>
2025-11-30 13:12:15 +0530

    Add fluid cursor

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AuroraBackground from './components/AuroraBackground.vue'
 import InteractiveHoverButton from './components/InteractiveHoverButton.vue'
+import FluidCursor from './components/FluidCursor.vue'
 </script>
 
 <template>
+  <FluidCursor />
   <AuroraBackground>
     <div class="z-10 flex flex-col items-center gap-4">
       <h1 class="text-4xl font-bold text-center md:text-6xl">
