0dc1b1488299b6efb2087e117cda31486b52181a
Ravi Chandra Anagani <ravi.chandra737@gmail.com>
2025-11-30 14:07:01 +0530

    Fix grayscale effect on mobile touch

--- src/components/DirectionAwareHover.vue
+++ src/components/DirectionAwareHover.vue
@@ -182,8 +182,9 @@ const imageClass = computed(() =>
     "scale-125", // Mobile
     "sm:scale-135", // Small screens
     "md:scale-150", // Desktop
-    // Grayscale to color effect
-    "grayscale group-hover/card:grayscale-0",
+    // Grayscale to color effect (desktop hover + mobile touch)
+    direction.value !== null || isTouched.value ? "grayscale-0" : "grayscale",
+    "group-hover/card:grayscale-0",
     props.imageClass,
   ),
 );
